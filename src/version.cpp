9d435786c221e69d1db9b578dd0026e504e582e4
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-11-06 22:17:41 +0700

    forgot add icon

--- src/version.cpp
+++ src/version.cpp
@@ -2,8 +2,8 @@
 
 namespace microservice
 {
-	const char* server_version::GIT_SHA1           = "aa1d26b638c7af962d501d081c7e656478c4a264-dirty";
+	const char* server_version::GIT_SHA1           = "fbe771bea0b33b506b67dec81a19e4f93edce7df-dirty";
 	const char* server_version::GIT_BRANCH         = "main";
-	const char* server_version::GIT_DATE           = "Mon Nov 6 18:36:48 2023";
-	const char* server_version::GIT_COMMIT_SUBJECT = "add git and product db entity";
+	const char* server_version::GIT_DATE           = "Mon Nov 6 21:44:31 2023";
+	const char* server_version::GIT_COMMIT_SUBJECT = "add database";
 };
