60105bfe983ca649992fe71c0c00978464441978
skript023 <fatur_muhammad@bk.ru>
2023-12-20 10:52:28 +0700

    exclude version

--- src/version.cpp
+++
@@ -1,9 +0,0 @@
-#include "version.hpp"
-
-namespace microservice
-{
-	const char* server_version::GIT_SHA1           = "13e8101c0f906d1a7b0e36012952b2307ed71ab1-dirty";
-	const char* server_version::GIT_BRANCH         = "main";
-	const char* server_version::GIT_DATE           = "Thu Nov 9 22:52:01 2023";
-	const char* server_version::GIT_COMMIT_SUBJECT = "fix some func on gcc";
-};
